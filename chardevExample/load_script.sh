4f558bf9af43416706d4b59737e58df608838535
Filip Brosman <xbrosman@stuba.sk>
2022-05-27 15:23:57 +0200

    Changed script to do make commands before.

--- chardevExample/load_script.sh
+++ chardevExample/load_script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
+make
 sudo insmod simple_module.ko
 sudo mknod -m 777 /dev/simple_chardev c 42 0
--- chardevExample/remove_script.sh
+++ chardevExample/remove_script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
+make clean
 sudo rmmod simple_module.ko
 sudo rm /dev/simple_chardev
