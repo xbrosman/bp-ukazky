d81b20eff28a60ad28c7a31b14a67571d14a6f31
Filip Brosman <xbrosman@stuba.sk>
2022-05-27 16:01:28 +0200

    Rename retun to res.

--- chardevExample/simple_app.c
+++ chardevExample/simple_app.c
@@ -12,10 +12,10 @@ double cpu_time_used;
 int fd = 0;
 int offset = 0;
 
-int writeToDev() {
+void writeToDev() {
     char *dataToWrite = (char*)malloc(SIZE*sizeof(char));
    
-    ssize_t ret;   
+    ssize_t res;   
     int i;
     for (i=0; i < SIZE;i++)
     {
@@ -25,36 +25,43 @@ int writeToDev() {
     int size = strlen(dataToWrite);   
     //printf("\nData na zapis: %s\n", dataToWrite);
 
-    ret = write(fd, dataToWrite, size, &offset);
+    clock_t t;
+    double time_taken;
 
-    if (ret == -1)
+    t = clock();
+    res = write(fd, dataToWrite, size, &offset);
+    t = clock() - t;
+    time_taken = ((double)t)/CLOCKS_PER_SEC;
+    printf("Cas na vykonanie zapisu: %fus\n", time_taken*1000000);
+
+    if (res == -1)
         printf("Zapisovanie sa nepodarilo...\n");
     
-    printf("Zapisanych: %liB ...\n", ret);
+    printf("Zapisanych: %liB ...\n", res);
     free(dataToWrite);
-    return 0;
 }
 
-int readFromDev() {
+void readFromDev() {
     char *data = (char*)malloc(SIZE*sizeof(char));
     memset(data, 0, sizeof(data));
-    data[0] = '\0';
-    ssize_t ret;     
+    ssize_t res;     
 
-    ret = read(fd, data, SIZE, &offset);
+    clock_t t;
+    double time_taken;
+    t = clock();
+    res = read(fd, data, SIZE, &offset);
+    t = clock() - t;
+    time_taken = ((double)t)/CLOCKS_PER_SEC;
+    printf("Cas na vykonanie citania: %fus\n", time_taken*1000000);
     //printf("\nCitanie z  %s: %s\n", DEVICE, data);
-    if (ret == -1)
+    if (res == -1)
         printf("citanie sa nepodarilo\n");
-    printf("Precitaných: %liB ... \n",ret);
+    printf("Precitaných: %liB ... \n",res);
     free(data);
-    return 0;
 }
 
 int main(int argc, char const *argv[])
 {
-    clock_t t;
-    double time_taken;
-
     if (access(DEVICE, F_OK) == -1){
     
         printf("Module %s not loaded... Close\n", DEVICE);
@@ -63,21 +70,11 @@ int main(int argc, char const *argv[])
     printf("Module %s loaded... \n", DEVICE);
 
     fd = open(DEVICE, O_RDWR);    
-    t = clock();
-    writeToDev();
-    t = clock() - t;
-    time_taken = ((double)t)/CLOCKS_PER_SEC;
-    printf("Cas na vykonanie zapisu: %fus\n", time_taken*1000000);
-    
+    writeToDev();  
     close(fd);
 
-    fd = open(DEVICE, O_RDWR);
-   
-    t = clock();
-     readFromDev();
-    t = clock() - t;
-    time_taken = ((double)t)/CLOCKS_PER_SEC;
-    printf("Cas na vykonanie citania: %fus\n", time_taken*1000000);
+    fd = open(DEVICE, O_RDWR);  
+    readFromDev();
     close(fd);
 
     return 0;
