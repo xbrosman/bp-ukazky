2f6ced1e7a14af53b529135592580fd7655318db
Filip Brosman <xbrosman@stuba.sk>
2022-06-02 20:04:49 +0200

    feat: Working netlink module.

--- netlinkExample/netlink_module.c
+++ netlinkExample/netlink_module.c
@@ -12,58 +12,57 @@
 #include <linux/skbuff.h>
 
 #define NAME "netlink_module"
-#define NETLINK_TEST 17
+#define NETLINK_PORT 17
 
 struct sock *nl_sock = NULL;
 
-static void netlink_test_recv_msg(struct sk_buff *skb)
+static void nl_rec_msg(struct sk_buff *skb)
 {
-    struct sk_buff *skb_out;
-    struct nlmsghdr *nlh;
     int msg_size;
     char *msg;
     int pid;
     int res;
+    struct sk_buff *skb_out;
+    struct nlmsghdr *nlh;
 
     nlh = (struct nlmsghdr *)skb->data;
-    pid = nlh->nlmsg_pid; /* pid of sending process */
+    pid = nlh->nlmsg_pid;
     msg = (char *)nlmsg_data(nlh);
     msg_size = strlen(msg);
 
-    printk(KERN_INFO "netlink_test: Received from pid %d: %s\n", pid, msg);
+    printk(KERN_INFO "%s: Received from pid %i: %s\n", NAME, pid, msg);
 
-    // create reply
     skb_out = nlmsg_new(msg_size, 0);
     if (!skb_out)
     {
-        printk(KERN_ERR "netlink_test: Failed to allocate new skb\n");
+        printk(KERN_ERR "%s: Failed to allocate new skb\n", NAME);
         return;
     }
 
-    // put received message into reply
     nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
-    NETLINK_CB(skb_out).dst_group = 0; /* not in mcast group */
+    NETLINK_CB(skb_out).dst_group = 0;
     strncpy(nlmsg_data(nlh), msg, msg_size);
 
-    printk(KERN_INFO "netlink_test: Send %s\n", msg);
+    printk(KERN_INFO "%s: Send %s\n", NAME, msg);
 
     res = nlmsg_unicast(nl_sock, skb_out, pid);
     if (res < 0)
-        printk(KERN_INFO "netlink_test: Error while sending skb to user\n");
+        printk(KERN_INFO "%s: Error while sending skb to user\n", NAME);
 }
+
+struct netlink_kernel_cfg nl_config = {
+    .input = nl_rec_msg,
+};
 
 int netlink_module_init(void)
 {
     printk(KERN_INFO "%s: %s\n", NAME, __FUNCTION__);
-    struct netlink_kernel_cfg nl_config = {
-        .input = netlink_test_recv_msg,
-    };
 
-    nl_sock = netlink_kernel_create(&init_net, NETLINK_TEST, &nl_config);
+    nl_sock = netlink_kernel_create(&init_net, NETLINK_PORT, &nl_config);
     if (!nl_sock)
     {
         printk(KERN_ALERT "%s: Error creating socket.\n", NAME);
-        return -10;
+        return -1;
     }
     return 0;
 }
