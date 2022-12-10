068ff2ca304e0eb9408829a70d4b17775ed754c3
Filip Brosman <xbrosman@stuba.sk>
2022-12-10 12:55:29 +0100

    fix build script, bad build check

--- vfs_example/buildRun.sh
+++ vfs_example/buildRun.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Building..."
-gcc vfs_app.c -o vfs_app.app
+gcc vfs_app.c -o vfs_app.app -g &
 echo "Build done..."
 echo "Run vfs_app.app...\n"
 sudo ./vfs_app.app
+echo "\nApp close.\n"
