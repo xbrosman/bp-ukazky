53306586c4be242d331148a9a2e29370c2c23544
Filip Brosman <xbrosman@stuba.sk>
2022-12-14 23:09:18 +0100

    change name of app

--- chardev_example/buildRun.sh
+++ chardev_example/buildRun.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "Building..."
-$appFile = "simple_app.app"
+$appFile = "simple_app"
 
 
 if [-f "$appFile"]; then
     rm simple_app.app
 fi
-gcc simple_app.c -o simple_app.app
+gcc simple_app.c -o simple_app
 echo "Build done..."
 echo "Run simple_app.app...\n"
-./simple_app.app
+./simple_app
