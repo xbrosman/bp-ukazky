21b1cb1d533de1d56e8656ea59a390d132b9e09b
Filip Brosman <xbrosman@stuba.sk>
2022-12-14 21:21:04 +0100

    add check if file exist in buildApp script

--- chardev_example/buildRun.sh
+++ chardev_example/buildRun.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 echo "Building..."
-rm simple_app.app
+$appFile = "simple_app.app"
+
+
+if [-f "$appFile"]; then
+    rm simple_app.app
+fi
 gcc simple_app.c -o simple_app.app
 echo "Build done..."
 echo "Run simple_app.app...\n"
