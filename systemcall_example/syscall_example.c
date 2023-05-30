ff5200580cd1cb445bcce0fb5d240b0e6a45d83f
Filip Brosman <xbrosman@stuba.sk>
2023-05-30 20:09:55 +0200

    added syscall example

---
+++ systemcall_example/syscall_example.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    pid_t pid;
+    int ret;
+
+    // vytvorenie noveho procesu a pid noveho procesu 
+    pid = fork();
+
+    if (pid == 0) {
+        // detsky proccess   
+        printf("Proccess: %d", getpid());
+    } else if (pid > 0) {
+        // rodičovsky proccess
+        printf("Proccess: %d", getpid());
+    }
+    else {
+        // Chyba pri vytvárani noveho procesu
+        perror("Error: fork");
+        return -1;
+    }
+
+    return 0;
+}
